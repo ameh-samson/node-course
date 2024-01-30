02e3711a230560f8b3d611515dc27634fff5c567
Samson Ameh <amehsam69@gmail.com>
2024-01-31 00:43:01 +0100

    send html page to browser

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 // creating the server
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  //   set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  // send an html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 // listening to request
